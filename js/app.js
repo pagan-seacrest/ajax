7a556b8995fd7b0db556bc96af1e478527c76eaa
pagan <pagansxc@pm.me>
2021-01-18 22:01:12 +0300

    start

--- js/app.js
+++ js/app.js
@@ -8,3 +8,41 @@ return fetch(req, { method: "GET" });
 
 
 console.log(episodes().then(data => data.json));
+
+const list = document.getElementById("ul");
+
+class Doc {
+    constructor (element) {
+        this.listElement = element;
+        this.textList = [];
+    }
+
+    static createListItem (text) {
+        const li = document.createElement("li");
+        li.textContent = text;
+
+        return li;
+    }
+
+    update () {
+        while (this.listElement.firstChild) {
+            this.listElement.removeChild(this.listElement.firstChild);
+        }
+
+        for (const text of this.textList) {
+            this.listElement.append(Doc.createListItem(text));
+        }
+    }
+
+    add (text) {
+        this.textList.push(text);
+        this.update();
+    }
+
+    remove (index) {
+        this.textList.splice(index, 1);
+        this.update();
+    }
+}
+
+let def = new Doc (10, 11);
